95192b0e110a251f4ea0877bc8ee27a8cdb1bb06
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-09-09 14:00:24 +0900

    Handle xml icons which are mistaken

--- lib/android_apk.rb
+++ lib/android_apk.rb
@@ -155,6 +155,17 @@ class AndroidApk
       icon.gsub! %r{res/(drawable|mipmap)-anydpi-(?:v\d+)/([^/]+)\.xml}, "res/\\1-#{dpis}-v4/\\2.png"
     end
 
+    # a fallback to a png file which is manually resolved
+    if want_png && icon.end_with?(".xml")
+      dpis = dpi_str(dpi)
+      icon.gsub! %r{res/(drawable|mipmap)/([^/]+)\.xml}, "res/\\1-#{dpis}-v4/\\2.png"
+    end
+
+    # we cannot prepare for any fallbacks
+    if want_png && icon.end_with?(".xml")
+      return nil
+    end
+
     Dir.mktmpdir do |dir|
       output_to = File.join(dir, icon)
 
@@ -164,7 +175,7 @@ class AndroidApk
         content = zip_file.find_entry(icon)&.get_input_stream&.read
         return nil if content.nil?
 
-        File.open(output_to, "wb") do |f|
+        File.open(output_to, "w") do |f|
           f.write(content)
         end
       end
